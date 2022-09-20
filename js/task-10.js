3b6d3ba12f14b05328fdb5a52300121ac90cbb96
dmstepanuik <0669218646@ukr.net>
2022-09-21 00:40:08 +0300

    task - 10 complete

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,50 @@
+const controlsRef = document.querySelector('#controls');
+const inputRef = controlsRef.querySelector('input');
+const createBtnRef = controlsRef.querySelector('[data-create]');
+const destroyBtnRef = controlsRef.querySelector('[data-destroy]');
+const boxesRef = document.querySelector('#boxes');
+
+inputRef.value = 0;
+
+createBtnRef.addEventListener('click', onCreateBtnClick);
+destroyBtnRef.addEventListener('click', onDestroyBtnClick);
+
+function onCreateBtnClick() {
+  createBoxes(inputRef.valueAsNumber);
+}
+function onDestroyBtnClick() {
+  clearElement(boxesRef);
+}
+
+function createBoxes(amount) {
+  clearElement(boxesRef);
+  boxesRef.insertAdjacentHTML('beforeend', getBoxListTemplate(amount));
+}
+
+function getBoxListTemplate(amount) {
+  const arr = [];
+  for (let i = 30; i <= 30 + (amount - 1) * 10; i = i + 10) {
+    arr.push(i);
+  }
+  return arr.map(it => getBoxItemTemplate(it)).join('');
+}
+
+function getBoxItemTemplate(size) {
+  const color = getRandomHexColor();
+  return `
+  <div
+    style='width:${size}px;
+    height:${size}px;
+    background-color:${color}'></div>
+  `;
+}
+
+function clearElement(el) {
+  while (el.childNodes.length > 0) {
+    el.firstChild.remove();
+  }
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
